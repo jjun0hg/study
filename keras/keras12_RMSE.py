c7808673817b0e9e05a6dc99158c501024ebe708
jjun0hg <jjun0hg@gmail.com>
2023-01-04 12:03:38 +0900

    keras

--- keras/keras12_RMSE.py
+++ keras/keras12_RMSE.py
@@ -1,3 +1,4 @@
+# RMSE
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import numpy as np
@@ -20,16 +21,29 @@ model.add(Dense(1))
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam', 
-              metrics=['mae', 'mse', 'accuracy', 'acc'])        #   metrics는 훈련에 영향을 미치진 않지만 값은 나온다.
+              metrics=['mae'])                                  #   metrics는 훈련에 영향을 미치진 않지만 값은 나온다.
                                                                 #   acc와 accuracy는 동일
-                                                                # error가 그 다음 가중치 갱신을 할때 영향을 미친다.(훈련에 영향을 미친다.)
-model.fit(x_train, y_train, epochs=200, batch_size=1)
+                                                                #   error가 그 다음 가중치 갱신을 할때 영향을 미친다.(훈련에 영향을 미친다.)
+model.fit(x_train, y_train, epochs=200, batch_size=1)           #   가중치 생성
 
 #4. 평가, 예측
-loss = model.evaluate(x_test, y_test)   #   test 데이터로 평가
+loss = model.evaluate(x_test, y_test)                           #   test 데이터로 평가
 print('loss : ', loss)
 
-# mae :  3.186760187149048
+y_predict = model.predict(x_test)                               #   최적의 가중치
 
-# mse :  15.0862398147583
+print("=============")
+print(y_test)
+print(y_predict)
+print("=============")
 
+# RMSE function
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))       #   np.sqrt = RMSE, mean_squared_error( , )= MSE
+
+print("RMSE : ", RMSE(y_test, y_predict))
+
+# RMSE :  3.86149851789604
+# RMSE :  3.8505546021288635
+# RMSE :  3.855078208869394
