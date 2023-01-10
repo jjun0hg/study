39b7820c6bb394f84c9aa64c80561b1418d56c92
jjun0hg <jjun0hg@gmail.com>
2023-01-10 15:02:17 +0900

    keras

--- keras/keras22_softmax3_digits.py
+++ keras/keras22_softmax3_digits.py
@@ -1,23 +1,28 @@
-# 와인을 감정하는 데이터
 import numpy as np
-from sklearn.datasets import load_wine
+from sklearn.datasets import load_digits
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.callbacks import EarlyStopping
 
 #1. 데이터
-datasets = load_wine()
+datasets = load_digits()
 x = datasets.data
-y = datasets.target
+y = datasets['target']
+print(x.shape, y.shape) # (1797, 64)    (1797, )    // 64 = 8*8*1(흑백)
+print(np.unique(y, return_counts=True))
+# (array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),       // y에 들어갈 칼럼은 하나지만 o_h encoding 하면 칼럼 10개로 늘어난다.
+#  array([178, 182, 177, 183, 181, 182, 181, 179, 174, 180], dtype=int64))          // 다중분류데이터(이미지 연산) // DNN방식
 
-print(x.shape, y.shape)    #   (178, 13) (178,)
-# print(y)
-print(np.unique(y))            # 라벨의 unique 한 값 //  y는[0 1 2]만 있다.
-print(np.unique(y, return_counts=True)) #   (array([0, 1, 2]), array([59, 71, 48], dtype=int64))
+# import matplotlib.pyplot as plt
+# plt.gray()
+# plt.matshow(datasets.images[4])
+# plt.show()
 
-from tensorflow.keras.utils import to_categorical
 y = to_categorical(y)
-print(y.shape)
+
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, shuffle=True,             
     random_state=333, 
@@ -27,18 +32,17 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 #2. 모델구성
 model = Sequential()
-model.add(Dense(50, activation='relu', input_shape=(13,)))
+model.add(Dense(50, activation='relu', input_shape=(64,)))
 model.add(Dense(40, activation='relu'))
 model.add(Dense(30, activation='relu'))
 model.add(Dense(20, activation='relu'))
 model.add(Dense(10, activation='linear'))
-model.add(Dense(3, activation='softmax'))
+model.add(Dense(10, activation='softmax'))
 
-#3. 컴파일, 훈련
-from tensorflow.keras.callbacks import EarlyStopping
 earlyStopping = EarlyStopping(monitor='val_loss', mode='min',
                               patience=30, restore_best_weights=True,
                               verbose=1) 
+
 model.compile(loss='categorical_crossentropy', optimizer='adam',    # sparse_categorical_crossentropy 로 변경해도 가능
               metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=1500, batch_size=32,
@@ -46,18 +50,15 @@ model.fit(x_train, y_train, epochs=1500, batch_size=32,
           validation_split=0.2,
           verbose=1)
 
-from sklearn.metrics import accuracy_score
-import numpy as np
-
 y_predict =  model.predict(x_test)
 # print(y_predict)
-y_predict = np.argmax(y_predict, axis = 1)                  # 가장 큰 자릿값 뽑아냄   / axis=1 (가로축(행)), axis=0 (세로축(열))
-print("y_pred(예측값) : ", y_predict)
+y_predict = np.argmax(y_predict, axis = 1)                 
+print("y_pred(예측값) : ", y_predict[:5])
 
 y_test = np.argmax(y_test, axis=1)
-print("y_test(원래값) : ", y_test)
+print("y_test(원래값) : ", y_test[:5])
 
-acc = accuracy_score(y_test, y_predict)                     # 소수점 들어가는 실수 형태로 구성// error 발생
+acc = accuracy_score(y_test, y_predict)
 print(acc)
 
-# 0.9722222222222222
+
