64ba84cf4dc1ddd5e26dcc495069bd76290f32d2
jjun0hg <jjun0hg@gmail.com>
2023-01-10 19:44:38 +0900

    keras/Binary_Classification

--- keras/keras20_sigmoid_cancer.py
+++ keras/keras20_sigmoid_cancer.py
@@ -41,6 +41,7 @@ hist = model.fit(x_train, y_train, epochs = 1000, batch_size=32,
           callbacks = [earlyStopping],
           verbose = 1)
 
+
 #4. 평가, 예측
 # loss = model.evaluate(x_test, y_test)
 # print('loss, accuracy : ', loss)
