3bd88ae1959a7add755318d965d9a3d6a3545dda
jjun0hg <jjun0hg@gmail.com>
2023-01-11 16:37:40 +0900

    keras/Multiclass_Classification

--- keras/keras22_softmax4_fetch_covtype.py
+++ keras/keras22_softmax4_fetch_covtype.py
@@ -11,36 +11,43 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 
 #1. 데이터
-datasets = fetch_covtype()
-x = datasets.data              # 데이터 뽑아올때 numpy로 변환되는건지??
+datasets = fetch_covtype()      # sklearn 데이터 셋 안에 numpy로 저장되어 있다.
+x = datasets.data               # numpy로 변환 되는 것이 아니다.
 y = datasets['target']
+print(type(x))
 # print(x.shape, y.shape)     #   (581012, 54)   (581012,)
 # print(np.unique(y, return_counts=True))     #   (array([1, 2, 3, 4, 5, 6, 7]), array([211840, 283301,  35754,   2747,   9493,  17367,  20510], dtype=int64))
 print(y.shape)
 
 ###################################################인코딩##############################################################
-# get_dummies       // .values or numpy 로 변환
-# y = pd.get_dummies(y, drop_first=False)
-# y = np.array(y)
+# get_dummies       // .values or numpy 로 변환 // index, header 자동생성 // numpy 자료형이 pandas를 바로 못받아들임
+y = pd.get_dummies(y, drop_first=False)
+# # y = y.values
+# # y = y.to_numpy()
+y = np.array(y)
 
 # to_categorical
-y = to_categorical(y)
-print(type(y))
-# print(y[:10])
-# print(np.unique(y[:,0], return_counts=True))            # 모든 행의 0번째
-y = np.delete(y, 0, axis=1)
+# y = to_categorical(y)
+# print(type(y))
+# # print(y[:10])
+# # print(np.unique(y[:,0], return_counts=True))            # 모든 행의 0번째
+# y = np.delete(y, 0, axis=1)
 
-# OneHotEncoder     // sparse=True default ==> Matrix 반환 // array가 필요하므로 False // .toarray()
-# Ohe = OneHotEncoder(sparse=False)
+# OneHotEncoder     preprocessing = 전처리 // sparse=True default ==> Matrix 반환 // array가 필요하므로 False // y = y.toarray()
+# Ohe = OneHotEncoder(sparse=True)
+# # y = y.reshape(581012, 1)
 # y = y.reshape(-1, 1)
-# print(y.shape)
-# Ohe.fit(y)                  
-# print(y.shape)
-# y = Ohe.transform(y)            # 원하는 형식으로 변환
-
+# # # print(y.shape)
+# # # Ohe.fit(y)                  
+# # # print(y.shape)
+# # y = Ohe.transform(y)            # 원하는 형식으로 변환 // 훈련시킨 결과(영향)에 대한 생성
+# y = Ohe.fit_transform(y)
+# y = y.toarray()
+# print(y[:15])
+# print(type(y))        scipy.sparse.~_maxtrix
 # sparse = True .toarray()      // False 그대로
 
-#########################################################################################################
+######################################################################################################################
 
 # num = np.unique(datasets['target'], axis=0)
 # num = num.shape[0]
@@ -70,12 +77,13 @@ earlyStopping = EarlyStopping(monitor='val_loss', mode='min',
 #3. 컴파일, 훈련
 model.compile(loss='categorical_crossentropy', optimizer='adam',    # sparse_categorical_crossentropy 로 변경해도 가능
               metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=1000, batch_size=64,
-          callbacks=[earlyStopping],
-          validation_split=0.2,
-          verbose=1)
-              
+model.fit(x_train, y_train, epochs=100, batch_size=64,
+        callbacks=[earlyStopping],
+        # validation_split=0.2,
+        verbose=1)
+
 # print(type(y))
+
 #4. 평가, 검증
 y_predict =  model.predict(x_test)
 # print(y_predict)
@@ -86,6 +94,5 @@ y_test = np.argmax(y_test , axis=1)
 print("y_test(원래값) : ", y_test[:20])
 
 acc = accuracy_score(y_test, y_predict)
-print(acc)      #0.8686264554271405
-
+print(acc)      #   0.8708725248057279
 
--- keras/keras25_sparse_categorical.py
+++ keras/keras25_sparse_categorical.py
@@ -1,4 +1,4 @@
-# 다중분류  //categorical_crossentropy
+# 다중분류 // sparse_categorical//iris data 
 from sklearn.datasets import load_iris
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -26,15 +26,15 @@ y = datasets['target'] # datasets.target
 # y = pd.get_dummies(y, drop_first=False)
 # y = np.array(y)
 
-from tensorflow.keras.utils import to_categorical
-y = to_categorical(y)
+# from tensorflow.keras.utils import to_categorical
+# y = to_categorical(y)
 # print(y.shape)        // (150,3)
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, shuffle=True,             # False의 문제점 = 동일한 값만 나올수 있다. (항상 데이터 확인) 
     #random_state=333, 
     test_size=0.2,
-    stratify=y                      # 분류형 데이터일 경우 가능하다
+    stratify=y                      # 분류형 데이터일 경우 가능하다(분류에서만 사용)
 )
 # print(y_train)
 # print(y_test)
@@ -48,7 +48,7 @@ model.add(Dense(20, activation='linear'))
 model.add(Dense(3, activation='softmax'))       # 다중분류 = Dense(3, softmax) y의 종류(class) 갯수
 
 #3. 컴파일, 훈련
-model.compile(loss='categorical_crossentropy', optimizer='adam',    # sparse_categorical_crossentropy 로 변경해도 가능
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',    # sparse_categorical_crossentropy 로 변경해도 가능
               metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=1, batch_size=3,
           validation_split=0.2,
@@ -63,24 +63,17 @@ print(y_test[:5])
 y_predict = model.predict(x_test[:5])
 print(y_predict)        # y_predict 결과값 모두 더하면 1
 
-from sklearn.metrics import accuracy_score
-import numpy as np
+# from sklearn.metrics import accuracy_score
+# import numpy as np
 
-y_predict =  model.predict(x_test)
-y_predict = np.argmax(y_predict, axis = 1)                  # 가장 큰 자릿값 뽑아냄   / axis=1 (가로축(행)), axis=0 (세로축(열))
-print("y_pred(예측값) : ", y_predict)
+# y_predict =  model.predict(x_test)
+# y_predict = np.argmax(y_predict, axis = 1)                  # 가장 큰 자릿값 뽑아냄   / axis=1 (가로축(행)), axis=0 (세로축(열))
+# print("y_pred(예측값) : ", y_predict)
 
-y_test = np.argmax(y_test, axis=1)                          # 원핫을 안했으니까 필요없다.
-print("y_test(원래값) : ", y_test)
+# y_test = np.argmax(y_test, axis=1)                          # np.argmax, acc 필요 x
+# print("y_test(원래값) : ", y_test)
 
-acc = accuracy_score(y_test, y_predict)                     # 소수점 들어가는 실수 형태로 구성// error 발생
-print(acc)
-
-
-
-
-
-
-
+# acc = accuracy_score(y_test, y_predict)                     # 소수점 들어가는 실수 형태로 구성// error 발생
+# print(acc)
 
 
