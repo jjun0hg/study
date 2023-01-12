41ed747be09595a714462a212513c0af8daac738
jjun0hg <jjun0hg@gmail.com>
2023-01-12 09:50:13 +0900

    keras/Binary_Classification

--- keras/keras20_sigmoid2_cancer.py
+++ keras/keras20_sigmoid2_cancer.py
@@ -34,26 +34,30 @@ model.add(Dense(10, activation='relu'))
 model.add(Dense(2, activation='softmax'))       # 0~1 사이의 값 출력
 
 #3.컴파일, 훈련
+
+# model.compile(loss='categorical_crossentropy', optimizer='adam',    # sparse_categorical_crossentropy 로 변경해도 가능
+#               metrics=['accuracy'])
+# model.fit(x_train, y_train, epochs=1000, batch_size=64,
+#           validation_split=0.2,
+#           verbose=1)
+# from tensorflow.keras.callbacks import EarlyStopping
+# earlyStopping = EarlyStopping(monitor='val_loss',
+#                               mode='min',
+#                               patience=30,
+#                               restore_best_weights=True,
+#                               verbose=1) 
 model.compile(loss='categorical_crossentropy', optimizer='adam',    # sparse_categorical_crossentropy 로 변경해도 가능
               metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=1000, batch_size=64,
-          validation_split=0.2,
-          verbose=1)
 from tensorflow.keras.callbacks import EarlyStopping
 earlyStopping = EarlyStopping(monitor='val_loss',
                               mode='min',
                               patience=30,
                               restore_best_weights=True,
                               verbose=1) 
-
-x_train, x_test, y_train, y_test = train_test_split(
-    x,y, shuffle=True, random_state=333, test_size=0.2
-)
-model.fit(x_train, y_train, epochs = 1000, batch_size=32,
+model.fit(x_train, y_train, epochs=1000, batch_size=32,
           validation_split=0.2,
-          callbacks = [earlyStopping],
-          verbose = 1)
-
+          callbacks=[earlyStopping],
+          verbose=1)
 
 #4. 평가, 예측
 # loss = model.evaluate(x_test, y_test)
