ae90009300887d0725e660a439115b0f3d09535d
jjun0hg <jjun0hg@gmail.com>
2023-01-13 18:30:00 +0900

    keras_CNN

--- keras/keras34_1_mnist.py
+++ keras/keras34_1_mnist.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tensorflow.keras.datasets import mnist
 
+#1. 데이터
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 print(x_train.shape, y_train.shape) #   (60000, 28, 28) (60000,)
@@ -12,19 +13,52 @@ x_test = x_test.reshape(10000, 28, 28, 1)
 print(x_train.shape, y_train.shape)
 print(x_test.shape, y_test.shape)
 
-print(np.unique(y_train, return_counts = True))     #(array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=uint8), array([5923, 6742, 5958, 6131, 5842, 5421, 5918, 6265, 5851, 5949],dtype=int64))
+print(np.unique(y_train, return_counts = True))     
+#(array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=uint8), array([5923, 6742, 5958, 6131, 5842, 5421, 5918, 6265, 5851, 5949],dtype=int64))
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
-
+#2. 모델
 model = Sequential()
 model.add(Conv2D(128, (2,2), input_shape=(28,28,1),         #(28, 28, 1)
                  activation='relu'))                        #(27, 27, 128) 
 model.add(Conv2D(64, (2,2)))                                #(26, 26, 64)
 model.add(Conv2D(64, (2,2)))                                #(25, 25, 64)
-model.add(Flatten())
-model.add(Dense(32, activation = 'relu'))
-model.add(Dense(10, activation = 'softmax'))
-model.summary()
+model.add(Flatten())                                        # 40000
+model.add(Dense(32, activation = 'relu'))                   # input_shape(40000,) //(60000,40000)// (batch_size, input_dim)
+model.add(Dense(10, activation = 'softmax'))                
 
+#3. 컴파일, 훈련
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer='adam',
+              metrics = ['acc'])
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss', mode='min',patience=10, 
+                  restore_best_weights=True,              
+                   verbose=1)
+import datetime                                             # 데이터 형식으로
+date = datetime.datetime.now()
+date = date.strftime("%m%d_%H%M")
+filepath = './_save/MCP/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
+mcp = ModelCheckpoint(monitor='val_loss', mode = 'auto', verbose = 1,
+                      save_best_only=True,
+                #      filepath = path + 'MCP/keras30_ModelCheckPoint3.hdf5')
+                      filepath = filepath + 'k34_01_' + date +'_'+ filename)
+
+
+
+model.fit(x_train, y_train, epochs=300, 
+            verbose= 1, 
+            batch_size=32,
+            validation_split=0.2,
+            callbacks=[es,mcp])
+
+#4. 평가, 예측
+result = model.evaluate(x_test, y_test)
+print('loss : ', result[0])
+print('acc : ', result[1])
+
+# loss :  0.10004928708076477
+# acc :  0.9708999991416931
 
