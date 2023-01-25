e98e470151d758c2392ac187eede00b3305b5330
jjun0hg <jjun0hg@gmail.com>
2023-01-25 14:39:10 +0900

    keras_CNN

--- keras/keras35_3_cifar10.py
+++ keras/keras35_3_cifar10.py
@@ -9,24 +9,13 @@ from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
 print(x_train.shape, y_train.shape) #   (50000, 32, 32, 3) (50000, 1)
 print(x_test.shape, y_test.shape)   #   (10000, 32, 32, 3) (10000, 1)
 
-model = Sequential()
-model.add(Conv2D(256, (3,3), input_shape=(32,32,3),         #(32, 32, 3)
-                padding="same",  
-                activation='relu'))                            #(27, 27, 128) 
-model.add(MaxPooling2D())        
-model.add(Conv2D(128, (3,3), activation='relu'))               #(26, 26, 64)
-model.add(Conv2D(64, (3,3), activation='relu'))                                #(25, 25, 64)
-model.add(Flatten())                                            # 40000
-model.add(Dense(32, activation = 'relu'))                      # input_shape(40000,) //(60000,40000)// (batch_size, input_dim)
-model.add(Dense(10, activation = 'softmax'))
-
 #2. 모델
 model = Sequential()
 model.add(Conv2D(256, (3,3), input_shape=(32,32,3),         #(32, 32, 3)
                 padding="same",  
-                strides= 2,
+                strides= 3,
                 activation='relu'))                            #(27, 27, 128) 
-model.add(MaxPooling2D()) 
+# model.add(MaxPooling2D()) 
 model.add(Conv2D(128, (3,3), activation='relu'))               #(26, 26, 64)
 model.add(Conv2D(64, (3,3), activation='relu'))                                #(25, 25, 64)
 model.add(Flatten())                                            # 40000
@@ -39,7 +28,7 @@ model.compile(loss = 'sparse_categorical_crossentropy', optimizer='adam',
               metrics = ['acc'])
 
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-es = EarlyStopping(monitor='val_loss', mode='min',patience=20, 
+es = EarlyStopping(monitor='val_loss', mode='min',patience=30, 
                   restore_best_weights=True,              
                    verbose=1)
 import datetime                                             # 데이터 형식으로
@@ -51,7 +40,7 @@ mcp = ModelCheckpoint(monitor='val_loss', mode = 'auto', verbose = 1,
                       save_best_only=True,
                       filepath = filepath + 'k35_03_' + date +'_'+ filename)
 
-model.fit(x_train, y_train, epochs=300, 
+model.fit(x_train, y_train, epochs=1000, 
             verbose= 1, 
             batch_size=250,
             validation_split=0.2,
@@ -62,3 +51,4 @@ result = model.evaluate(x_test, y_test)
 print('loss : ', result[0])
 print('acc : ', result[1])
 
+# 
--- keras/keras35_4_cifar100.py
+++ keras/keras35_4_cifar100.py
@@ -11,17 +11,20 @@ print(x_test.shape, y_test.shape)   #   (10000, 32, 32, 3) (10000, 1)
 
 print(np.unique(y_train, return_counts = True))   
 
+# import matplotlib.pyplot as plt
+# plt.imshow(x_train[10], 'gray')
+# plt.show()
+
 #2. 모델
 model = Sequential()
 model.add(Conv2D(512, (3,3), input_shape=(32,32,3),     
                 padding="same",  
-                use_bias=True,            
-                kernel_initializer="random_normal",
+                strides= 1,
                 activation='relu'))                      
-model.add(MaxPooling2D()) 
+# model.add(MaxPooling2D()) 
 model.add(Conv2D(256, (3,3), activation='relu'))
 model.add(Conv2D(128, (3,3), activation='relu'))
-model.add(Conv2D(64, (2,2), activation='relu'))
+model.add(Conv2D(64, (3,3), activation='relu'))
 model.add(Flatten())                                           # 40000
 model.add(Dense(32, activation = 'relu'))                      # input_shape(40000,) //(60000,40000)// (batch_size, input_dim)
 model.add(Dense(100, activation = 'softmax'))                  
@@ -41,7 +44,6 @@ filepath = './_save/MCP/'
 filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
 mcp = ModelCheckpoint(monitor='val_loss', mode = 'auto', verbose = 1,
                       save_best_only=True,
-                #      filepath = path + 'MCP/keras30_ModelCheckPoint3.hdf5')
                       filepath = filepath + 'k35_04_' + date +'_'+ filename)
 model.fit(x_train, y_train, epochs=1000, 
             verbose= 1, 
